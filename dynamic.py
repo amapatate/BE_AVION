03b68fe376b5acd2a0bd31f0c20ebaf16917acd8
amapatate <amapatate@gmx.fr>
2016-05-29 08:21:27 +0200

    Initial commit

---
+++ dynamic.py
@@ -0,0 +1,271 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+'''
+Dynamic model for a 3 Degrees Of Freedom longitudinal aircraft  
+'''
+import math, numpy as np, scipy.optimize
+import matplotlib.pyplot as plt
+
+import utils as ut
+
+''' naming of state and input components '''
+s_y, s_h, s_va, s_a, s_th, s_q, s_size = range(0, 7)
+i_dm, i_dth, i_wy, i_wz, i_size = range(0, 5)
+
+
+def get_mach(va, T, k=1.4, Rs=287.05): return va / math.sqrt(k * Rs * T)
+
+
+def va_of_mach(m, h, k=1.4, Rs=287.05):
+    p, rho, T = ut.isa(h)
+    return m * math.sqrt(k * Rs * T)
+
+
+def propulsion_model(X, U, P):
+    p, rho, T = ut.isa(X[s_h])
+    rho0 = 1.225
+    mach = get_mach(X[s_va], T)
+    return P.F0 * math.pow(rho / rho0, 0.6) * (0.568 + 0.25 * math.pow(1.2 - mach, 3)) * U[i_dth]
+
+
+# def get_aero_ceofs(h, va, alpha, q, dphr, P):
+#     St_over_S = P.St/P.S
+#     CL0 = (St_over_S*0.25*P.CLat - P.CLa)*P.a0
+#     CLa = P.CLa + St_over_S*P.CLat*(1-0.25)
+#     CLq = P.lt * St_over_S * P.CLat * P.CLq
+#     CLdphr = St_over_S*P.CLat
+#     CL = CL0 + CLa*alpha + CLq*q/va + CLdphr * dphr
+#     CD = P.CD0 + P.ki*CL**2
+#     Cm = P.Cm0 - P.ms*P.CLa + P.Cmq*P.lt/va*q + P.Cmd*dphr
+#     return CL, CD, Cm
+
+def get_aero_ceofs(va, alpha, q, dphr, P):
+    St_over_S = P.St / P.S
+    CL0 = (St_over_S * 0.25 * P.CLat - P.CLa) * P.a0
+    CLa = P.CLa + St_over_S * P.CLat * (1 - 0.25)
+    CLq = P.lt * St_over_S * P.CLat * P.CLq
+    CLdphr = St_over_S * P.CLat
+    CL = CL0 + CLa * alpha + CLq * q / va + CLdphr * dphr
+    CD = P.CD0 + P.ki * CL ** 2
+    Cm = P.Cm0 - (P.ms * P.CLa)(alpha - P.a0) + P.Cmq * P.lt / va * q + P.Cmd * dphr
+    return CL, CD, Cm
+
+
+def get_aero_forces_and_moments(X, U, P):
+    p, rho, T = ut.isa(X[s_h])
+    pdyn = 0.5 * rho * X[s_va] ** 2
+    CL, CD, Cm = get_aero_ceofs(X[s_h], X[s_va], X[s_a], X[s_q], U[i_dm], P)
+    L, D, M = pdyn * P.S * np.array([CL, CD, P.cbar * Cm])
+    return L, D, M
+
+
+def dyn(X, t, U, P):
+    '''  Dynamic model '''
+    Xdot = np.zeros(s_size)
+    gamma_a = X[s_th] - X[s_a]  # air path angle
+    cg, sg = math.cos(gamma_a), math.sin(gamma_a)
+    ca, sa = math.cos(X[s_a]), math.sin(X[s_a])
+    L, D, M = get_aero_forces_and_moments(X, U, P)
+    F = propulsion_model(X, U, P)
+    Xdot[s_y] = X[s_va] * cg - U[i_wy]
+    Xdot[s_h] = X[s_va] * sg - U[i_wz]
+    Xdot[s_va] = (F * ca - D) / P.m - P.g * sg
+    Xdot[s_a] = X[s_q] - (L + F * sa) / P.m / X[s_va] + P.g / X[s_va] * cg
+    Xdot[s_th] = X[s_q]
+    Xdot[s_q] = M / P.Iyy
+    return Xdot
+
+
+def trim(P, args=None):
+    va = args.get('va', 100.)
+    gamma = args.get('gamma', 0.)
+    h = args.get('h', 5000.)
+    wy, wz = 0, 0
+
+    def err_func(arg):
+        (dm, dth, alpha) = arg
+        theta = gamma + alpha
+        U = np.array([dm, dth, wy, wz])
+        X = np.array([0., h, va, alpha, theta, 0])
+        L, D, M = get_aero_forces_and_moments(X, U, P)
+        F = propulsion_model(X, U, P)
+        cg, sg = math.cos(gamma), math.sin(gamma)
+        ca, sa = math.cos(alpha), math.sin(alpha)
+        return [(F * ca - D) / P.m - P.g * sg, -(L + F * sa) / P.m + P.g * cg, M]
+
+    p0 = [ut.rad_of_deg(0.), 0.5, ut.rad_of_deg(1.)]
+    sol = scipy.optimize.root(err_func, p0, method='hybr')
+    dm, dth, alpha = sol.x
+    X, U = [0, h, va, alpha, gamma + alpha, 0], [dm, dth, 0, 0]
+    return X, U
+
+
+class Param:
+    def __init__(self):
+        self.g = 9.81
+        self.m_k = 0.5
+        self.ms = 0.3  # static margin
+        # aero
+        self.a0 = ut.rad_of_deg(-2.)  # zero lift angle
+        self.CD0 = 0.025  # zero drag coefficient
+        self.Cm0 = -0.59  # zero moment coefficient
+        self.CLq = 1.3
+
+    def set_mass_and_static_margin(self, km, sm):
+        self.m_k = km
+        self.ms = sm
+        self.compute_auxiliary()
+
+    def compute_auxiliary(self):
+        self.m = (1 - self.m_k) * self.m_OWE + self.m_k * self.m_MTOW
+        self.Iyy = 0.5 * (1. / 12. * self.m * self.l_fus ** 2)
+
+        self.lt = 0.5 * self.l_fus  # CG to tail distance
+
+        self.CLa = math.pi * self._lambda / (1. + math.sqrt(1 + (0.5 * self._lambda) ** 2))
+        self.ki = 1. / (math.pi * self._lambda)
+
+        self.CLat = math.pi * self._lambdat / (1. + math.sqrt(1 + (0.5 * self._lambdat) ** 2))
+
+        Vt = self.lt * self.St / self.cbar / self.S  # tail volume
+        self.Cmd = -Vt * self.CLat
+        self.Cmq = self.Cmd * self.CLq
+
+
+class Param_A320(Param):
+    def __init__(self, m_k=0.5):
+        Param.__init__(self)
+        self.name = 'Airbus A-320'
+        self.m_OWE = 39733.
+        self.m_MTOW = 73500.
+        self.m_k = m_k
+
+        self.l_fus = 37.57  # length of fuselage
+        self.cbar = 4.19  # wing reference chord
+        self.St = 31.  # tail lifting surface
+        self.S = 122.44  # wing surface
+        self._lambdat = 5.  # tail aspect ratio
+        self._lambda = 9.39  # wing aspect ratio
+
+        self.F0 = 2. * 111205  # engine max thrust
+        self.eng_name = 'CFM 56-5A1'
+
+        self.compute_auxiliary()
+
+
+class Param_737_800(Param):
+    def __init__(self, ):
+        Param.__init__(self)
+        self.name = 'Boeing 737-800'
+        self.m_OWE = 41413.
+        self.m_MTOW = 70534.
+
+        self.l_fus = 38.02  # length of fuselage
+        self.cbar = 4.17  # wing reference chord
+        self.St = 32.8  # tail lifting surface
+        self.S = 124.6  # wing surface
+        self._lambdat = 6.28  # tail aspect ratio
+        self._lambda = 9.45  # wing aspect ratio
+
+        self.F0 = 2. * 106757  # engine max thrust
+        self.eng_name = 'CFM 56-7B24'
+
+        self.compute_auxiliary()
+
+
+class Param_A319(Param):
+    def __init__(self):
+        Param.__init__(self)
+        self.name = 'Airbus A-319'
+        self.m_OWE = 39358.
+        self.m_MTOW = 64000.
+
+        self.l_fus = 33.84  # length of fuselage
+        self.cbar = 4.19  # wing reference chord
+        self.St = 31.0  # tail lifting surface
+        self.S = 122.44  # wing surface
+        self._lambdat = 5.  # tail aspect ratio
+        self._lambda = 9.39  # wing aspect ratio
+
+        self.F0 = 2. * 97860  # engine max thrust
+        self.eng_name = 'CFM 56-5B5'
+
+        self.compute_auxiliary()
+
+
+class Param_A321(Param):
+    def __init__(self):
+        Param.__init__(self)
+        self.name = 'Airbus A-321'
+        self.m_OWE = 47000.
+        self.m_MTOW = 89000.
+
+        self.l_fus = 44.51  # length of fuselage
+        self.cbar = 4.34  # wing reference chord
+        self.St = 31.0  # tail lifting surface
+        self.S = 126.00  # wing surface
+        self._lambdat = 5.  # tail aspect ratio
+        self._lambda = 9.13  # wing aspect ratio
+
+        self.F0 = 2. * 133446  # engine max thrust
+        self.eng_name = 'CFM 56-5B1'
+
+        self.compute_auxiliary()
+
+
+class Param_737_700(Param):
+    def __init__(self):
+        Param.__init__(self)
+        self.name = 'Boeing 737-700'
+        self.m_OWE = 37648.
+        self.m_MTOW = 60326.
+
+        self.l_fus = 32.18  # length of fuselage
+        self.cbar = 4.17  # wing reference chord
+        self.St = 32.8  # tail lifting surface
+        self.S = 124.60  # wing surface
+        self._lambdat = 6.28  # tail aspect ratio
+        self._lambda = 9.44  # wing aspect ratio
+
+        self.F0 = 2. * 61633  # engine max thrust
+        self.eng_name = 'CFM 56-7B20'
+
+        self.compute_auxiliary()
+
+
+class Param_737_300(Param):
+    def __init__(self):
+        Param.__init__(self)
+        self.name = 'Boeing 737-300'
+        self.m_OWE = 31480.
+        self.m_MTOW = 56473.
+
+        self.l_fus = 32.18  # length of fuselage
+        self.cbar = 3.73  # wing reference chord
+        self.St = 31.31  # tail lifting surface
+        self.S = 91.04  # wing surface
+        self._lambdat = 5.15  # tail aspect ratio
+        self._lambda = 9.16  # wing aspect ratio
+
+        self.F0 = 2. * 88694  # engine max thrust
+        self.eng_name = 'CFM 56-3B1'
+
+        self.compute_auxiliary()
+
+
+all_ac_types = [Param_A320, Param_737_800, Param_A319, Param_A321, Param_737_700, Param_737_300]
+
+
+def plot(time, X, U=None, figure=None, window_title="Trajectory"):
+    figure = ut.prepare_fig(figure, window_title, (20.48, 10.24))
+    plots = [("$y$", "m", X[:, s_y], None),
+             ("$h$", "m", X[:, s_h], 2.),
+             ("$v_a$", "m/s", X[:, s_va], 1.),
+             ("$\\alpha$", "deg", ut.deg_of_rad(X[:, s_a]), 2.),
+             ("$\\theta$", "deg", ut.deg_of_rad(X[:, s_th]), 2.),
+             ("$q$", "deg/s", ut.deg_of_rad(X[:, s_q]), 2.)]
+    for i, (title, ylab, data, min_yspan) in enumerate(plots):
+        ax = plt.subplot(3, 2, i + 1)
+        plt.plot(time, data)
+        ut.decorate(ax, title=title, ylab=ylab, min_yspan=min_yspan)
+    return figure
---
+++ utils.py
@@ -0,0 +1,134 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+'''
+
+Utilities
+
+'''
+import math, numpy as np
+import matplotlib.pyplot as plt
+import pdb
+
+"""
+Unit convertions
+"""
+
+
+def rad_of_deg(d): return d / 180. * math.pi
+
+
+def deg_of_rad(r): return r * 180. / math.pi
+
+
+# http://en.wikipedia.org/wiki/Nautical_mile
+def m_of_NM(nm): return nm * 1852.
+
+
+def NM_of_m(m): return m / 1852.
+
+
+# http://en.wikipedia.org/wiki/Knot_(speed)
+def mps_of_kt(kt): return kt * 0.514444
+
+
+def kt_of_mps(mps): return mps / 0.514444
+
+
+"""
+International Standard Atmosphere Model
+see: http://en.wikipedia.org/wiki/International_Standard_Atmosphere
+"""
+_name, _h0, _z0, _a, _T0, _p0 = np.arange(6)
+#      name         h(m)    z(km)    a(K/m)    T0(K)     p0(Pa)
+isa_param = \
+    [['Troposphere', 0, 0.0, -6.5e-3, 288.15, 101325],
+     ['Tropopause', 11000, 11.019, 0.0e-3, 216.65, 22632],
+     ['Stratosphere', 20000, 20.063, 1.0e-3, 216.65, 5474.9],
+     ['Stratosphere', 32000, 32.162, 2.8e-3, 228.65, 868.02],
+     ['Stratopause', 47000, 47.350, 0.0e-3, 270.65, 110.91],
+     ['Mesosphere', 51000, 51.413, -2.8e-3, 270.65, 66.939],
+     ['Mesosphere', 71000, 71.802, -2.0e-3, 214.65, 3.9564],
+     ['Mesopause', 84852, 86.000, 0., 186.87, 0.3734]]
+
+
+def isa(h):
+    layer = 0
+    while isa_param[layer][_h0] < h: layer += 1
+    if layer == 0: layer = 1  # in case h<= 0
+    name, h0, z0, a, T0, p0 = isa_param[layer - 1]
+    dh = h - h0
+    T = T0 + a * dh
+    g, R = 9.81, 287.05
+    if a != 0.:
+        p = p0 * math.pow(T / T0, -g / a / R)
+    else:
+        p = p0 * math.exp(-g / R / T0 * dh)
+    rho = p / R / T
+    return p, rho, T
+
+
+"""
+Compute numerical jacobian 
+"""
+
+
+def num_jacobian(X, U, P, dyn):
+    s_size = len(X)
+    i_size = len(U)
+    epsilonX = (0.1 * np.ones(s_size)).tolist()
+    dX = np.diag(epsilonX)
+    A = np.zeros((s_size, s_size))
+    for i in range(0, s_size):
+        dx = dX[i, :]
+        delta_f = dyn(X + dx / 2, 0, U, P) - dyn(X - dx / 2, 0, U, P)
+        delta_f = delta_f / dx[i]
+        A[:, i] = delta_f
+
+    epsilonU = (0.1 * np.ones(i_size)).tolist()
+    dU = np.diag(epsilonU)
+    B = np.zeros((s_size, i_size))
+    for i in range(0, i_size):
+        du = dU[i, :]
+        delta_f = dyn(X, 0, U + du / 2, P) - dyn(X, 0, U - du / 2, P)
+        delta_f = delta_f / du[i]
+        B[:, i] = delta_f
+
+    return A, B
+
+
+"""
+Plotting
+"""
+
+
+def decorate(ax, title=None, xlab=None, ylab=None, legend=None, xlim=None, ylim=None, min_yspan=None):
+    ax.xaxis.grid(color='k', linestyle='-', linewidth=0.2)
+    ax.yaxis.grid(color='k', linestyle='-', linewidth=0.2)
+    if xlab: ax.xaxis.set_label_text(xlab)
+    if ylab: ax.yaxis.set_label_text(ylab)
+    if title: ax.set_title(title, {'fontsize': 20})
+    if legend != None: ax.legend(legend, loc='best')
+    if xlim != None: ax.set_xlim(xlim[0], xlim[1])
+    if ylim != None: ax.set_ylim(ylim[0], ylim[1])
+    if min_yspan != None: ensure_yspan(ax, min_yspan)
+
+
+def ensure_yspan(ax, yspan):
+    ymin, ymax = ax.get_ylim()
+    if ymax - ymin < yspan:
+        ym = (ymin + ymax) / 2
+        ax.set_ylim(ym - yspan / 2, ym + yspan / 2)
+
+
+def prepare_fig(fig=None, window_title=None, figsize=(20.48, 10.24), margins=None):
+    if fig == None:
+        fig = plt.figure(figsize=figsize)
+    else:
+        plt.figure(fig.number)
+    if margins:
+        left, bottom, right, top, wspace, hspace = margins
+        fig.subplots_adjust(left=left, right=right, bottom=bottom, top=top,
+                            hspace=hspace, wspace=wspace)
+    if window_title:
+        fig.canvas.set_window_title(window_title)
+    return fig
