8140bb32d04af180e9b862e7872cad3b2785347e
amapatate <amapatate@gmx.fr>
2016-06-07 09:15:35 +0200

    séance 3 Q4

--- seance3.py
+++ seance3.py
@@ -8,15 +8,11 @@ import dynamic as dy
 from scipy.integrate import odeint
 from pylab import *
 
-
-
-
 markeur = ('o', 'v', '^', '<', '>', '1', '2', '3', '4', '8', 's', 'p', '*', 'h', 'H', '+', 'x', 'D', 'd', ' ')
 plt.grid(True)
 plt.axhline()
 plt.axvline()
 P = dy.Param_737_300()
-
 
 h_tuple = (3000., 11000.)
 h_kilometre = np.array(h_tuple) / 1000
@@ -29,17 +25,11 @@ abs_min = 3
 abs_max = 11
 nb_ligne = len(km_tuple) * len(ms_tuple)
 
-
 # on choisit le point de trim numéro 16 pour lequel on a :
 h = 11000.
 mac = 0.8
 km = 0.9
 ms = 1.
-
-
-
-
-
 
 
 # def etat_lin4(h_tuple, mac_tuple, km_tuple, ms_tuple):
@@ -119,11 +109,10 @@ def v_propre(h_tuple, mac_tuple, km_tuple, ms_tuple):
                     dico_etat_lin[pt_trim] = etat_lin
                     dico_etat_lin4[pt_trim] = etat_lin4
                     dico_vp[pt_trim] = vp[0]
-                    dico_vecteur_propre[pt_trim] = vp[1:][0] # car vp est un tuple ; indice 1 => array des vect propre indice 0 val propres
+                    dico_vecteur_propre[pt_trim] = vp[1:][
+                        0]  # car vp est un tuple ; indice 1 => array des vect propre indice 0 val propres
 
-
-    return dico_vp, dico_vecteur_propre,dico_etat_lin4, dico_etat, dico_etat_lin
-
+    return dico_vp, dico_vecteur_propre, dico_etat_lin4, dico_etat, dico_etat_lin
 
 
 # *********************************************************************************************************************
@@ -138,8 +127,8 @@ tout = v_propre(h_tuple, mac_tuple, km_tuple, ms_tuple)  # on récupere "tout"
 vp = tout[0]  # on choisit uniquement les val propres, pas les vecteurs
 
 # $$$ on fixe un pt de trim
-pt_trim =  (3000.0, 0.5, 0.9, 1.)
-vap1=vp[pt_trim] # on choisit les val propres d'un seul point de trim
+pt_trim = (11000.0, 0.5, 0.9, 1.)
+vap1 = vp[pt_trim]  # on choisit les val propres d'un seul point de trim
 # print(type(vap1))
 vep = tout[1]  # on selectionne le dico des vecteurs propres
 M = vep[pt_trim]  # on selectionne la matrice de vect propre associés au trim choisi ; M matrice de passage tq X = MX'
@@ -150,34 +139,32 @@ A4B4 = A4B4_dict[pt_trim]
 A4 = A4B4[0]
 B4 = A4B4[1]
 XU_dico = tout[3]
-X,U = XU_dico[pt_trim]
+X, U = XU_dico[pt_trim]
 X = np.array(X)
-U=np.array(U)
+U = np.array(U)
 
 # print(type(U))
 
-#récup de A et B
+# récup de A et B
 AB_dict = tout[4]
 AB = AB_dict[pt_trim]
 A = AB[0]
 B = AB[1]
 
 
-
 def affiche_mat(M):
     try:
-        nb_col=len(M[:,0])
-        nb_ligne=len(M[0,:])
+        nb_col = len(M[:, 0])
+        nb_ligne = len(M[0, :])
         for l in range(0, nb_ligne):
             for c in range(0, nb_col):
-                f = "{:+.5f}  ".format(M[l,c])
-                print(f,end='')
+                f = "{:+.5f}  ".format(M[l, c])
+                print(f, end='')
             print()
-    except :
+    except:
         for l in range(0, len(M)):
             f = "{:+.5f}  ".format(M[l])
             print(f)
-
 
 
 # Pour la séance 3, le point de trim
@@ -189,22 +176,21 @@ def affiche_mat(M):
 # -0.00269042 - 0.05730786j
 # ]
 
-#5.2.1 - évolution sur 240s suite à une rafale de vent verticale de wh = 2 m/s induisant un nouvelle angle alpha
+# 5.2.1 - évolution sur 240s suite à une rafale de vent verticale de wh = 2 m/s induisant un nouvelle angle alpha
 # initial à partir duquel on "lache" l'avion.
 
-#calcul du nouveau vecteur d'état intial on a alpha
-#atan(wh/vae)
-dalpha =  math.atan(2./X[dy.s_va])
-Xi=X.copy()
-Ui=np.copy(U)
-Xi[dy.s_a]+=dalpha
-
+# calcul du nouveau vecteur d'état intial on a alpha
+# atan(wh/vae)
+dalpha = math.atan(2. / X[dy.s_va])
+Xi = X.copy()
+Ui = np.copy(U)
+Xi[dy.s_a] += dalpha
 
 print("*******************")
 
-#X est l'état d'equilibre
-dXi=np.zeros(6)
-dXi[dy.s_a]=dalpha
+# X est l'état d'equilibre
+dXi = np.zeros(6)
+dXi[dy.s_a] = dalpha
 A = np.array(A)
 # def dyn_lin(dX,t):
 #
@@ -213,7 +199,6 @@ A = np.array(A)
 #     return dXdot
 
 Xe = X.copy()
-
 
 
 ##########################################################################"
@@ -226,12 +211,12 @@ Xe = X.copy()
 
 ###########################################################################
 
-def dyn_lin(X,t):
-    dXdot=np.dot(A,X)-np.dot(A,Xe)
+def dyn_lin(X, t):
+    dXdot = np.dot(A, X) - np.dot(A, Xe)
     return dXdot
 
 
-def Xet(X,nb):
+def Xet(X, nb):
     '''
     produit
     :param X:
@@ -239,20 +224,20 @@ def Xet(X,nb):
     :return:un array contitué de nb lignes du vecteur X
     '''
     col = len(X)
-    xet = np.zeros((nb,col))
+    xet = np.zeros((nb, col))
     for i in range(nb):
-        xet[i,:]= X
+        xet[i, :] = X
     return xet
 
 
 def trajectoire(X, U, P):
-    tt=240# 240s
-    nb_val=1000
+    tt = 240  # 240s
+    nb_val = 1000
     t = np.linspace(0, tt, nb_val)
     sol = odeint(dy.dyn, X, t, (U, P))
     sol_lin = odeint(dyn_lin, X, t)
 
-    dy.plot2(t, sol,sol_lin, U=None, figure=None, window_title="Paramètres d'état en fonction du temps(secondes)")
+    dy.plot2(t, sol, sol_lin, U=None, figure=None, window_title="Paramètres d'état en fonction du temps(secondes)")
     # dy.plot(t, soli, U=None, figure=None, window_title="Paramètres d'état en fonction du temps(secondes)")
     plt.suptitle("Paramètres d'état en fonction du temps(secondes)- trim = {}".format(pt_trim))
     plt.savefig('seance3/param_of_time.png', dpi=120)  # sauvegarde du graphe au format png dans le dossier images
@@ -271,6 +256,7 @@ def trajectoire(X, U, P):
     # # plt.savefig('seance3/trajectoire.png', dpi=120)  # sauvegarde du graphe au format png dans le dossier images
     # plt.show()
 
+
 # trajectoire(Xi,U,P)
 
 
@@ -280,26 +266,26 @@ def trajectoire(X, U, P):
 # 5.2.4 - donner la repr modale
 
 # modèle linéaire 4 composantes
-dXi=np.zeros(4)
-dXi[1]=dalpha
+dXi = np.zeros(4)
+dXi[1] = dalpha
 
-#calcul de M-1 l'inverse de M
-M_1=np.linalg.inv(M)
-dXip = np.dot(M_1,dXi)
+# calcul de M-1 l'inverse de M
+M_1 = np.linalg.inv(M)
+dXip = np.dot(M_1, dXi)
 print("*****************************************************************")
 print("La matrice de passage est M = ")
 print(affiche_mat(M))
 print("*****************************************************************")
-#vap1 contient les 4 val propres
-diag4=np.diag(vap1) #np.exp(vap1)
+# vap1 contient les 4 val propres
+diag4 = np.diag(vap1)  # np.exp(vap1)
 
 print("Dans la base propre A est diagonale et vaut : ")
 print(affiche_mat(diag4))
 print()
 print("La matrice de passage inverse M_1 = ")
-print(affiche_mat(M_1) )
+print(affiche_mat(M_1))
 print()
-B4p = np.dot(M_1,B4)
+B4p = np.dot(M_1, B4)
 print("La matrice B4p = M_1*B4 dans la base propre est :")
 print(affiche_mat(B4p))
 print()
@@ -308,7 +294,40 @@ print()
 print("A4 = ", A4)
 print()
 print("B4 = ", B4)
-# print("\n\n\n*****************************")
+
+###########################################################################################
+print()
+print("STABILITE DU SYSTEME LINEAIRE A4 B4 ? CAS LTI CNS DE STABILITE asymptotique AST RE(VP)<0");
+print("si otutes les vp à partie réelles nulles sont non dégénérées le sys est NAST stable NON asymptotiquement")
+print()
+
+print("valeurs propres ", vap1)
+
+print("CONCLUSION : LE MODELE LINEAIRE A 4 COMPOSANTE EST INSTABLE POUR LE PT_TRIM ", pt_trim)
+print("les modes")
+
+
+
+
+# print("On calcule les modules des valeurs propres ; si tous les modules sont < 1, le système est stable.")
+# print("Les vp de A à 4 composantes sont : ")
+# print()
+# print(vap1)
+#
+# print("valeurs propres ", vap1)
+#
+# mod_car = np.power(np.real(vap1), 2)+ np.power(np.imag(vap1), 2)
+# print("modules des valeurs propres :  ")
+# print(np.power(mod_car,0.5))
+
+
+
+
+
+
+
+
+
 
 ########################################################################
 # Pour chaque pas de temps contenu dans t, on calcule les dXp
@@ -329,7 +348,7 @@ print("B4 = ", B4)
 # dy.plot2(t, sol,sol2, U=None, figure=None, window_title="Paramètres d'état en fonction du temps(secondes)")
 # plt.show()
 
-#dX = X(t)-Xe=X(t)-X => X(t) = X+dX
+# dX = X(t)-Xe=X(t)-X => X(t) = X+dX
 
 ############################################################################################################"
 # seconde méthode
@@ -342,7 +361,3 @@ print("B4 = ", B4)
 #     '''
 #     dXdot = np.dot(A,dX)
 #     Xdot= X+
-
-
-
-
